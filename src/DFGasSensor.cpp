2d7c12cd1624772ae2a93834d980a732f09c138b
Garth Johnson <growlf@thenetyeti.com>
2024-09-26 19:14:23 -0700

    initial import

---
+++ src/DFGasSensor.cpp
@@ -0,0 +1,37 @@
+#include "DFGasSensor.h"
+
+/// @brief Creates a gas sensor
+/// @param I2CAddress The I2C address of the sensor
+DFGasSensor::DFGasSensor(int I2CAddress) : Gas_Sensor(&Wire, I2CAddress) {
+	GAS_I2C_ADDRESS = I2CAddress;
+}
+
+/// @brief Starts a gas sensor
+/// @return True on success
+bool DFGasSensor::begin() {
+	values.resize(1);
+	Description.parameterQuantity = 1;
+	Description.type = "Environmental Sensor";
+	Description.name = "Gas Sensor";
+	Description.parameters = {"Gas"};
+	Description.units = {"ppm"};
+	Description.id = 4;
+	// Start the sensor
+	if (Gas_Sensor.begin()) {
+		if (Gas_Sensor.changeAcquireMode(Gas_Sensor.PASSIVITY)) {
+			Gas_Sensor.setTempCompensation(Gas_Sensor.ON);
+			String GasType = Gas_Sensor.queryGasType();
+			Description.name = GasType + " Sensor";
+			Description.parameters = {GasType};
+			return true;
+		}
+	}
+	return false;
+}
+
+/// @brief Take a gas measurement and stores it in an internal variable
+/// @return True on success
+bool DFGasSensor::takeMeasurement() {
+	values[0] = Gas_Sensor.readGasConcentrationPPM();
+	return values[0] != 0.0;
+}	
---
+++ src/DFGasSensor.h
@@ -0,0 +1,37 @@
+/*
+* This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman
+* Contributors: Sam Groveman
+* 
+* External libraries needed:
+* DFRobot_MultiGasSensor: https://github.com/DFRobot/DFRobot_MultiGasSensor
+* 
+* Gas sensors: https://www.dfrobot.com/product-2510.html
+* 
+* I2C address select via dip-switch, default to 0x74, A1 and A0 are grouped into 4 I2C addresses.
+*             | A0 | A1 |
+*             | 0  | 0  |    0x74 default I2C address  
+*             | 0  | 1  |    0x75
+*             | 1  | 0  |    0x76
+*             | 1  | 1  |    0x77
+*/
+
+#pragma once
+#include <Arduino.h>
+#include <DFRobot_MultiGasSensor.h>
+#include <Wire.h>
+#include <Sensor.h>
+
+class DFGasSensor: public Sensor {
+	public:        
+		DFGasSensor(int I2CAddress);
+		bool begin();
+		bool takeMeasurement();
+
+	private:
+		/// @brief I2C address of gas sensor sensor
+		int GAS_I2C_ADDRESS;
+
+		/// @brief Gas sensor object
+		DFRobot_GAS_I2C Gas_Sensor;
+
+};
